34278882a2f2433f8890c784109a95e41e6820a2
Kiha Ahn <ahnkiha@icloud.com>
2026-04-02 10:46:20 +0900

    Refactor shared editor controls and project save flow

--- apps/web/src/components/dialogs/LibraryBrowserDialog.tsx
+++ apps/web/src/components/dialogs/LibraryBrowserDialog.tsx
@@ -298,7 +298,7 @@ export function LibraryBrowserDialog<T>({
 
   return (
     <Dialog open={open} onOpenChange={onOpenChange}>
-      <DialogContent className="h-[min(calc(100vh-2.5rem),960px)] max-h-[min(calc(100vh-2.5rem),960px)] w-[calc(100vw-2.5rem)] max-w-none border-none bg-transparent p-4 shadow-none sm:p-6">
+      <DialogContent className="h-[min(calc(100vh-2.5rem),960px)] max-h-[min(calc(100vh-2.5rem),960px)] w-[calc(100vw-2.5rem)] max-w-none border-none bg-transparent p-4 shadow-none sm:max-w-none sm:p-6">
         <div className="flex h-full min-h-0 flex-col overflow-hidden rounded-[32px] border border-border/70 bg-background shadow-[0_40px_120px_-52px_rgba(15,23,42,0.58)]">
           <div className="shrink-0 border-b border-border/70 px-6 py-6 pr-16">
             <DialogHeader className="gap-2 text-left">
